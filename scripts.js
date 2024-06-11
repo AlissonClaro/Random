f68a77ca69d3850141972ab0fa3e7446fc8dba53
AlissonClaro <allnclaro@outlook.com>
2024-06-11 11:04:52 -0300

    edit aulas

--- scripts.js
+++ scripts.js
@@ -4,6 +4,11 @@ function generateNumber() {
     const numMin = Math.ceil(document.querySelector('.input-min').value)
     const numMax = Math.floor(document.querySelector('.input-max').value)
 
+    if (numMax <= numMin) {
+        alert('O numero maximo é menor ou igual ao numero minimo ❌, tente outra vez ')        
+    } 
+    else {
     const result = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
     document.getElementById('result').textContent = 'O número sorteado é: ' + result +' 👈🏻';
+    }
 }
